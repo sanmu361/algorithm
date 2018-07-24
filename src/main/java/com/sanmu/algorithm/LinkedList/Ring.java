d9890ff266e47b4c8d2e2741bf3074e0ace185c4
sanmu361 <1535958735@qq.com>
2018-07-24 19:13:56 +0800

    新代码

--- src/main/java/com/sanmu/algorithm/LinkedList/Ring.java
+++ src/main/java/com/sanmu/algorithm/LinkedList/Ring.java
@@ -16,4 +16,44 @@ public class Ring extends AbstractLinkedList{
     protected void doSomeThing(Node list) {
 
     }
+
+    public void judgeRing(Node list){
+        Node fast = list;
+        Node slow = list;
+
+        if(list == null){
+            return ;
+        }
+
+        while(true){
+            if(slow.getNext() != null && fast.getNext() != null && fast.getNext().getNext() != null){
+                slow = slow.getNext();
+                fast = fast.getNext().getNext();
+            }else{
+                return;
+            }
+
+            if(fast == slow){
+                System.out.println(fast);
+            }
+        }
+    }
+
+    public int ringLength(Node list,Node ringMeetNode){
+        Node fast = list;
+        Node slow = ringMeetNode;
+
+        int length = 0;
+        while(true){
+            fast = fast.getNext();
+            slow = slow.getNext();
+            length++;
+            if(fast == slow){
+                break;
+            }
+        }
+        return length;
+    }
+
+
 }
