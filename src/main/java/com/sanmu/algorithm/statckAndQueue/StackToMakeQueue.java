6bfa8c8a876dde76bd27282ebfad0d566f710316
sanmu361 <1535958735@qq.com>
2018-08-02 10:33:45 +0800

    新代码

--- src/main/java/com/sanmu/algorithm/statckAndQueue/StackToMakeQueue.java
+++ src/main/java/com/sanmu/algorithm/statckAndQueue/StackToMakeQueue.java
@@ -1,5 +1,7 @@
 package com.sanmu.algorithm.statckAndQueue;
 
+import org.apache.commons.collections.CollectionUtils;
+
 import java.util.Stack;
 
 /**
@@ -17,6 +19,45 @@ public class StackToMakeQueue {
     private static class Queue<T>{
         private final Stack<T> stack1 = new Stack<T>();
         private final Stack<T> stack2 = new Stack<T>();
+
+        public boolean add(T t){
+            while(CollectionUtils.isNotEmpty(stack2)){
+                stack1.push(stack2.pop());
+            }
+            return stack1.add(t);
+        }
+
+        public boolean offer(T t){
+            return add(t);
+        }
+
+        public T remove(){
+            while(CollectionUtils.isNotEmpty(stack1)){
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+
+        public T poll(){
+            while(CollectionUtils.isNotEmpty(stack1)){
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+
+        public T element(){
+            while(CollectionUtils.isNotEmpty(stack1)){
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
+        }
+
+        public T peek(){
+            while(CollectionUtils.isNotEmpty(stack1)){
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
+        }
 
 
     }
